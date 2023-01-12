d0dfb16ae84a83e7e2a656b1341a1e7a8c79647d
IDEALOGIC\khoa-dang.pham <khoa.pham@publishone.nl>
2023-01-12 18:36:00 +0700

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Elykp.IdentityServer.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public string Hello()
+        {
+            return "Hello world";
+        }
+    }
+}
